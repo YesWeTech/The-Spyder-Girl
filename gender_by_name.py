3a5dd59c70fa212e38bf8b71ae50b138c9cae44d
mgmacias95 <mgmacias95@correo.ugr.es>
2017-03-10 21:58:24 +0100

    getting our feed gender stats!

--- gender_by_name.py
+++ gender_by_name.py
@@ -1,16 +1,28 @@
 import genderator
 from get_fb_names import *
+from collections import OrderedDict
 
 def guess_gender_name(name, guesser):
     answer = guesser.guess_gender(name)
     if answer:
-        print(answer)
+        return answer
     else:
-        print('Name doesn\'t match')
+        return OrderedDict([('names', name), ('gender', 'Unknown')])
+
+def export_to_json(data_list):
+    results = {'Female':0, 'Male':0, 'Unknown':0}
+
+    for d in data_list:
+        results[d['gender']] += 1
+
+    return results
 
 
 names = list(get_feed_names().keys())
 guesser = genderator.Parser()
+data_list = []
 
 for name in names:
-    guess_gender_name(name, guesser)
+    data_list.append(guess_gender_name(name, guesser))
+
+export_to_json(data_list)
