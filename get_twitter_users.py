0e2093dd08a1f32d5785fa38e232894a38ffd76c
mgmacias95 <mgmacias95@correo.urg.es>
2017-03-11 13:02:21 +0100

    implemented cursor

--- get_twitter_users.py
+++ get_twitter_users.py
@@ -34,11 +34,8 @@ api = tweepy.API(auth_handler=auth, wait_on_rate_limit_notify=True,
 def get_time_line_retweeters():
 
     tweets_list = []
-    # get the tweets of our timeline
-    timeline = api.user_timeline(
-        by_screenname='geekantechgirl', count=200, include_rts=False)
     # construct the dataframe
-    for tweet in timeline:
+    for tweet in tweepy.Cursor(api.user_timeline, include_rts=False).items():
 
         tweets_list.append([str(tweet.id), int(tweet._json['favorite_count']),
                             str(tweet._json['retweet_count']), tweet.created_at])
