aabf6507383933c4f72614994089a93e87a241f2
Luis <hfegetude@gmail.com>
2017-03-11 11:11:08 +0100

    env python

--- get_twitter_users.py
+++ get_twitter_users.py
@@ -1,14 +1,13 @@
-import tweepy # Manage Twitter API
+#! /usr/bin/env python
 from os import environ # To get the environment variables
+import tweepy # Manage Twitter API
+
 
 # Use your Twitter API keys to connect 
+auth = tweepy.OAuthHandler(environ["GIRL_KEY"], environ["GIRL_SECRET"])
+auth.set_access_token(environ["GIRL_ACCESS_TOKEN"], environ["GIRL_ACCESS_SECRET"])
 
-auth = tweepy.OAuthHandler(environ["TWITTER_CONSUMER_KEY"], 
-                           environ["TWITTER_CONSUMER_SECRET"])
-
-auth.set_access_token(environ["TWITTER_ACCESS_TOKEN"], 
-                      environ["TWITTER_ACCESS_TOKEN_SECRET"])
-
-api = tweepy.API(auth_handler=auth, wait_on_rate_limit_notify=True, 
-wait_on_rate_limit=True)
-
+api = tweepy.API(auth_handler=auth, wait_on_rate_limit_notify=True, wait_on_rate_limit=True)
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
